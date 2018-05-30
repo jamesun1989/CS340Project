6cbe5ee27882dd6a8f28a2488a6902278fae0c9e
jamesun1989 <jamesun1989@gmail.com>
2018-05-29 17:58:53 -0700

    add cpucooler to navbar

--- main.php
+++ main.php
@@ -7,7 +7,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
 	<script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="bootstrap.techie.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap.techie.min.css">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 

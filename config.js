c9ed8b468baf5e8aa0f1f05333e1adfdec101fbb
hammockt <hammocktrevor@gmail.com>
2018-06-04 15:03:04 -0700

    Add the new endpoints

--- config.js
+++ config.js
@@ -1,17 +1,20 @@
 var CONFIG =
 {
 	authToken: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/authToken",
-    refreshToken: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/refreshToken",
+	refreshToken: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/refreshToken",
 
-    createAccount : "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/createAccount",
-    
-    getComputerCases: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/getComputerCases",
-    getCPU: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/getCPUs",
-    getCPUCoolers: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/getCPUCoolers",
-    getGraphicsCards: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/getGraphicsCards",
-    getMotherboards: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/getMotherboards",
-    getPowerSupplies: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/getPowerSupplies",
-    getRAM: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/getRAM",
-    getStorage: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/getStorage",
-    getUserBuild: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/getUserBuild"
+	createAccount: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/createAccount",
+	updateUserProperties: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/updateUserProperties",
+
+	getComputerCases: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/getComputerCases",
+	getCPU: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/getCPUs",
+	getCPUCoolers: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/getCPUCoolers",
+	getGraphicsCards: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/getGraphicsCards",
+	getMotherboards: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/getMotherboards",
+	getPowerSupplies: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/getPowerSupplies",
+	getRAM: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/getRAM",
+	getStorage: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/getStorage",
+
+	listUserBuilds: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/listUserBuilds",
+	getUserBuild: "https://web.engr.oregonstate.edu/~hammockt/cs340/Project/Dev/API/getUserBuild"
 };

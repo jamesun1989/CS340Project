dccf1fd894db876a543ebf4509d2eb6cac24f3ea
hammockt <hammocktrevor@gmail.com>
2018-06-11 13:31:53 -0700

    Add logout functionallity

--- nav.js
+++ nav.js
@@ -2,13 +2,14 @@ $(document).ready(function()
 {
 	$('.navbar a').each(function()
 	{
-		let fileName = this.href.split('/').pop();
+		let fileName = this.href.split(/(\/|#)/).pop();
 
 		if(localStorage.getItem('refreshToken') === null)
 		{
 			switch(fileName)
 			{
 				case "listBuilds.php": $(this).addClass('hidden'); break;
+				case "logout": $(this).addClass('hidden'); break;
 			};
 		}
 		else
@@ -18,6 +19,17 @@ $(document).ready(function()
 				case "login.php":
 				case "signup.php": $(this).addClass('hidden'); break;
 			}
+
+			$('#logout').click(function(e)
+			{
+				e.preventDefault();
+
+				localStorage.removeItem('refreshToken');
+				localStorage.removeItem('authToken');
+				localStorage.setItem('loginRedirect', 'main.php');
+
+				window.location.replace("main.php");
+			});
 		}
 	});
 });
